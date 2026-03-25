32b928117e8355fbcd236c4c25b3803cc9148e1f
Anil Bal <anilbal7676@gmail.com>
2026-03-25 23:40:39 +0545

    interface add

--- src/components/elements/icons/Icons.tsx
+++ src/components/elements/icons/Icons.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
-interface IconProps{
-  props?:React.SVGProps<SVGSVGElement>;
-}
+export interface IconProps extends React.SVGProps<SVGSVGElement> {}
+
 export const PreviewIcon: React.FC<IconProps> = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -21,7 +20,7 @@ export const PreviewIcon: React.FC<IconProps> = (props) => (
   </svg>
 );
 
-export const ExportIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+export const ExportIcon: React.FC<IconProps> = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="16"
@@ -40,16 +39,38 @@ export const ExportIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   </svg>
 );
 
-export const NavbarSectionIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
+export const NavbarSectionIcon: React.FC<IconProps> = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
     <rect width="20" height="6" x="2" y="3" rx="1" />
     <path d="M6 6h.01" />
     <path d="M10 6h.01" />
   </svg>
 );
 
-export const HeroSectionIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
+export const HeroSectionIcon: React.FC<IconProps> = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
     <rect width="20" height="12" x="2" y="6" rx="2" />
     <path d="M2 12h20" />
     <path d="M7 18v-2" />
@@ -57,8 +78,19 @@ export const HeroSectionIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) 
   </svg>
 );
 
-export const FeaturesSectionIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
+export const FeaturesSectionIcon: React.FC<IconProps> = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
     <path d="M3 10h18" />
     <path d="M3 14h18" />
     <path d="M3 18h18" />
@@ -66,19 +98,40 @@ export const FeaturesSectionIcon: React.FC<React.SVGProps<SVGSVGElement>> = (pro
   </svg>
 );
 
-export const CTASectionIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
+export const CTASectionIcon: React.FC<IconProps> = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
     <path d="m5 14 7-3 7 3V5a2 2 0 0 0-2-2H7a2 2 0 0 0-2 2Z" />
     <path d="M12 19v2" />
     <path d="M9 21h6" />
   </svg>
 );
 
-export const FooterSectionIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
+export const FooterSectionIcon: React.FC<IconProps> = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
     <rect width="20" height="6" x="2" y="15" rx="1" />
     <path d="M6 18h.01" />
     <path d="M10 18h.01" />
   </svg>
 );
-
